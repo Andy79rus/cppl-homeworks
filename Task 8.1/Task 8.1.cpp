871ac67b66fa019c77cccb6ffe6c116532ac4391
Andrey Serov <andy79rus@gmail.com>
2022-09-02 16:06:10 +0200

    Task 8.1

---
+++ Task 8.1/Task 8.1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	std::vector<int> vec = { 1, 1, 2, 5, 6, 1, 2, 4 };
+	std::vector<int> vec2 = { 1, 8, 1, 6, 2, 2, 3, 5, 5, 5, 2, 4, 1, 6, 2, 3, 10, 6, 2, 5, 6, 3, 7 };
+
+	auto print = [](const std::vector<int>& v) {
+		for (const auto& i : v)
+			std::cout << i << ' ';
+		std::cout << '\n';
+	};
+
+	std::cout << "[IN]: ";
+	print(vec);
+
+	std::sort(vec.begin(), vec.end());
+	auto not_unique = std::unique(vec.begin(), vec.end());
+	vec.erase(not_unique, vec.end());
+
+	std::cout << "[OUT]: ";
+	print(vec);
+
+	std::cout << "[IN]: ";
+	print(vec2);
+
+	std::sort(vec2.begin(), vec2.end());
+	not_unique = std::unique(vec2.begin(), vec2.end());
+	vec2.erase(not_unique, vec2.end());
+
+	std::cout << "[OUT]: ";
+	print(vec2);
+
+	return 0;
+}
